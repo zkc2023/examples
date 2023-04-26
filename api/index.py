61621b239100c71937fa54cda085f56b1450a612
zkc2023 <131240503+zkc2023@users.noreply.github.com>
2023-04-26 14:46:45 +0800

    222

--- api/index.py
+++ api/index.py
@@ -8,6 +8,23 @@ canning = [
         ]
 
 
+template = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    '南大米', '北大米', '顺溜', '牛蛙', '饺子', '陈香贵', '干锅', '麻辣烫', '肯德基', '老乡鸡', '再抽一次',
+    '陕老顺', '那个绿色的店'
+<br/>
+<a href="/chisha">今天吃什么</a><br/>
+今天吃：{{XXXX}}
+</body>
+</html>
+'''
+
 @app.route('/index')
 def index():
     return 'hhhh'
@@ -16,4 +33,4 @@ def index():
 @app.route('/chisha')
 def chisha():
     num = randint(0, len(canning) - 1)
-    return canning[num]
+    return template.replace('{{XXXX}}',  canning[num])
